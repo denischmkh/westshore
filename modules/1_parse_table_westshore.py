cd60c2921507d3b84a1acbfb850ca912b29e97c0
denischmkh <avraam.l2004@gmail.com>
2025-05-28 13:11:33 +0300

    Project

--- modules/1_parse_table_westshore.py
+++ modules/1_parse_table_westshore.py
@@ -18,14 +18,8 @@ EMAIL_TO = 'osvspotfixtures@gmail.com'
 
 url = 'https://westshore.no/'
 
-browser = Chrome()
 
-browser.get(url=url)
-time.sleep(3)
 
-table_block = browser.find_element(by=By.XPATH, value='//tbody[@data-reactid=".0.1.0.0.1.1.1.1:$0.0.0.2.0.0.0.1"]')
-
-rows = table_block.find_elements(by=By.XPATH, value='./tr')
 
 def send_notification_email(subject, body):
     msg = MIMEMultipart()
@@ -42,38 +36,48 @@ def send_notification_email(subject, body):
         print("📧 Email has been send successfully.")
     except Exception as e:
         print(f"❌ Error while sending email: {e}")
+while True:
+    browser = Chrome()
+    browser.get(url=url)
+    time.sleep(3)
 
-for row in rows:
-    tds = [el.text for el in row.find_elements(by=By.XPATH, value='./td')]
-    date, vessel, charterer, scope_of_work, comm, rate = tds
-    print(f'Date: {date}')
-    print(f'Vessel: {vessel}')
-    print(f'Charterer: {charterer}')
-    print(f'Scope of Work: {scope_of_work}')
-    print(f'Comm.: {comm}')
-    print(f'Rate: {rate}')
-    print(f'')
-    record, created = CharterRecordWestshore.objects.get_or_create(
-        date=date,
-        vessel=vessel,
-        charterer=charterer,
-        scope_of_work=scope_of_work,
-        comm=comm,
-        rate=rate
-    )
-    if created:
-        subject = f"Westshore: {vessel}"
-        body = (
-            f"Date: {date}\n"
-            f"Vessel: {vessel}\n"
-            f"Charterer: {charterer}\n"
-            f"Scope of Work: {scope_of_work}\n"
-            f"Comm.: {comm}\n"
-            f"Rate: {rate}\n"
+    table_block = browser.find_element(by=By.XPATH, value='//tbody[@data-reactid=".0.1.0.0.1.1.1.1:$0.0.0.2.0.0.0.1"]')
+
+    rows = table_block.find_elements(by=By.XPATH, value='./tr')
+
+    for row in rows:
+        tds = [el.text for el in row.find_elements(by=By.XPATH, value='./td')]
+        date, vessel, charterer, scope_of_work, comm, rate = tds
+        print(f'Date: {date}')
+        print(f'Vessel: {vessel}')
+        print(f'Charterer: {charterer}')
+        print(f'Scope of Work: {scope_of_work}')
+        print(f'Comm.: {comm}')
+        print(f'Rate: {rate}')
+        print(f'')
+        record, created = CharterRecordWestshore.objects.get_or_create(
+            date=date,
+            vessel=vessel,
+            charterer=charterer,
+            scope_of_work=scope_of_work,
+            comm=comm,
+            rate=rate
         )
-        print(f"✅ New Record: {vessel}")
-        send_notification_email(subject, body)
-    else:
-        print(f"ℹ️ Already exists: {vessel}")
+        if created:
+            subject = f"Westshore: {vessel}"
+            body = (
+                f"Date: {date}\n"
+                f"Vessel: {vessel}\n"
+                f"Charterer: {charterer}\n"
+                f"Scope of Work: {scope_of_work}\n"
+                f"Comm.: {comm}\n"
+                f"Rate: {rate}\n"
+            )
+            print(f"✅ New Record: {vessel}")
+            send_notification_email(subject, body)
+        else:
+            print(f"ℹ️ Already exists: {vessel}")
+    browser.close()
+    time.sleep(10800)
 
 
--- modules/3_parse_table_howerobinsonoffshore.py
+++ modules/3_parse_table_howerobinsonoffshore.py
@@ -35,62 +35,65 @@ def send_notification_email(subject, body):
 
 url = 'https://www.howerobinsonoffshore.no/'
 
-browser = Chrome()
 
-browser.maximize_window()
+while True:
+    browser = Chrome()
 
-browser.get(url=url)
-time.sleep(5)
+    browser.maximize_window()
 
-is_first = True
-for _ in range(3):
-    if not is_first:
-        browser.find_element(by=By.XPATH, value='//div[@class="chakra-button css-1df3zeo"]').click()
-        time.sleep(5)
-    table_block = browser.find_element(by=By.XPATH, value='//table[@class="chakra-table sui-data-grid css-5gxp1o"]/tbody')
-    browser.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", table_block)
-    time.sleep(5)
+    browser.get(url=url)
+    time.sleep(10)
+    is_first = True
+    for _ in range(3):
+        if not is_first:
+            browser.find_element(by=By.XPATH, value='//div[@class="chakra-button css-1df3zeo"]').click()
+            time.sleep(10)
+        table_block = browser.find_element(by=By.XPATH, value='//table[@class="chakra-table sui-data-grid css-5gxp1o"]/tbody')
+        browser.execute_script("arguments[0].scrollIntoView({ behavior: 'smooth', block: 'center' });", table_block)
+        time.sleep(10)
 
-    rows = table_block.find_elements(by=By.XPATH, value='./tr')
+        rows = table_block.find_elements(by=By.XPATH, value='./tr')
 
-    for row in rows:
-        tds = [el.text for el in row.find_elements(by=By.XPATH, value='./td')]
-        date, vessel, type, charterer, scope_of_work, period, commencement, rate = tds
-        print(f'Date: {date}')
-        print(f'Vessel: {vessel}')
-        print(f'Type: {type}')
-        print(f'Charterer: {charterer}')
-        print(f'Scope of Work: {scope_of_work}')
-        print(f'Period: {period}')
-        print(f'Commencement: {commencement}')
-        print(f'Rate: {rate}')
-        print(f'')
-        record, created = CharterRecordHowerobinsonoffshore.objects.get_or_create(
-            date=date,
-            vessel=vessel,
-            type=type,
-            charterer=charterer,
-            scope_of_work=scope_of_work,
-            period=period,
-            commencement=commencement,
-            rate=rate
-        )
-        if created:
-            subject = f"Howerobinson: {vessel}"
-            body = (
-                f"Date: {date}\n"
-                f"Vessel: {vessel}\n"
-                f"Type: {type}\n"
-                f"Charterer: {charterer}\n"
-                f"Scope of Work: {scope_of_work}\n"
-                f"Period: {period}\n"
-                f"Commencement: {commencement}\n"
-                f"Rate: {rate}\n"
+        for row in rows:
+            tds = [el.text for el in row.find_elements(by=By.XPATH, value='./td')]
+            date, vessel, type, charterer, scope_of_work, period, commencement, rate = tds
+            print(f'Date: {date}')
+            print(f'Vessel: {vessel}')
+            print(f'Type: {type}')
+            print(f'Charterer: {charterer}')
+            print(f'Scope of Work: {scope_of_work}')
+            print(f'Period: {period}')
+            print(f'Commencement: {commencement}')
+            print(f'Rate: {rate}')
+            print(f'')
+            record, created = CharterRecordHowerobinsonoffshore.objects.get_or_create(
+                date=date,
+                vessel=vessel,
+                type=type,
+                charterer=charterer,
+                scope_of_work=scope_of_work,
+                period=period,
+                commencement=commencement,
+                rate=rate
             )
-            print(f"✅ New Record: {vessel}")
-            send_notification_email(subject, body)
-        else:
-            print(f"ℹ️ Already exists: {vessel}")
-    if not is_first:
-        break
-    is_first = False
+            if created:
+                subject = f"Howerobinson: {vessel}"
+                body = (
+                    f"Date: {date}\n"
+                    f"Vessel: {vessel}\n"
+                    f"Type: {type}\n"
+                    f"Charterer: {charterer}\n"
+                    f"Scope of Work: {scope_of_work}\n"
+                    f"Period: {period}\n"
+                    f"Commencement: {commencement}\n"
+                    f"Rate: {rate}\n"
+                )
+                print(f"✅ New Record: {vessel}")
+                send_notification_email(subject, body)
+            else:
+                print(f"ℹ️ Already exists: {vessel}")
+        if not is_first:
+            break
+        is_first = False
+    browser.close()
+    time.sleep(10800)
